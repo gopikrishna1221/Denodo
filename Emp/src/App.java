fd8fe2027b512545dac3685f2c03e4f5d690dd2f
gsarvadevabhatla <gsarvadevabhatla@PHL-R90GPH8F-L1.DAVITA.Corp>
2017-04-26 15:13:44 -0400

    added new print

--- Emp/src/App.java
+++ Emp/src/App.java
@@ -4,6 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Emp WebService");
+		System.out.println("Emp WebService2");
 	}
 
 }
